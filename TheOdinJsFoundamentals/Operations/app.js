297db85e309344f9034bc3edf3c59c888525ef45
tomaspf9 <tomaspf9@gmail.com>
2023-08-09 18:04:57 -0300

    Create Repo

---
+++ TheOdinJsFoundamentals/Operations/app.js
@@ -0,0 +1,29 @@
+
+/// Console logs de operaciones basicas
+console.log(23 + 97)
+console.log(2 + 4 + 5 / 13 * 2 + 10)
+console.log((4 + 6 + 9 )/ 77)
+
+/// Variables
+
+let a = 10
+
+console.log(a)
+
+console.log(a*9)
+
+let b = 7 * a
+
+console.log(b)
+
+
+/// Constantes
+
+
+const max = 57
+
+const actual = max - 13
+
+const percentage = actual / max 
+
+console.log(percentage)
